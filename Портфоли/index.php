0bcfaa0f46f9eef80c66bfcf2b02935924d4d6ef
Dima <dimamatveev121@gmail.com>
2020-06-25 23:05:46 +0500

    Это наш первый комит

---
+++ Портфоли/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Сайт портфолио</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/media.css">
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+	</head>
+	<body>
+	<header class="header">
+		<h1 class="header-title">Матвеев Дмитрий</h1>
+		<p class="header-subtitle">фрилансер, веб-разработчик, создание сайтов</p>
+		<a href="#portfolio" class="header-arrow">
+			<img src="img/Size.svg" alt="">
+		</a>
+	</header>
+	<main class="">
+		<section class="portfolio" id="portfolio">
+		<div class="container">
+		<h2 class="section-haider">Портфолио</h2>
+		<div class="portfolio-cards-wrapper">
+			<article class="cards">
+				<img class="card-img" src="img/Cards/forest.jpg" alt="Многостраничный сайт">
+				<h3>Многостраничный сайт</h3>
+				<p>Верстка многостраничного сайта.</p>
+			</article>
+			<article class="cards">
+				<img class="card-img" src="img/Cards/unit.jpg" alt="Многостраничный сайт">
+				<h3>Адаптивный лендинг</h3>
+				<p>Верстка многостраничного сайта.</p>
+			</article>
+			<article class="cards">
+				<img class="card-img" src="img/Cards/portafolio.jpg" alt="Многостраничный сайт">
+				<h3>Личный сайт</h3>
+				<p>Верстка многостраничного сайта.</p>
+			</article>
+			<article class="cards">
+				<img class="card-img" src="img/Cards/quantum.jpg" alt="Многостраничный сайт">
+				<h3>Интернет магазин</h3>
+				<p>Верстка многостраничного сайта.</p>
+			</article>
+		</div>
+		</div>
+		</section>
+	</main>
+	<footer class="footer ">
+		<div class="container">
+			<div class="footer-row">
+				<div class="footer-copyright">
+				<div class="name">© Дмитрий Матвеев</div>
+			<p>HTML верстка и разработка сайтов</p>
+		    </div>
+				<div class="footer-socials"><p>Мои профили в социальных сетях:</p>
+						<ul class="footer-socials-list">
+							<li><a href="#"><img src="img/socials/inst.svg" alt="instagrem"></a></li>
+							<li><a href="#"><img src="img/socials/facebook.svg" alt="facebook"></a></li>
+							<li><a href="#"><img src="img/socials/vk.svg" alt="vk"></a></li>
+							<li><a href="#"><img src="img/socials/linkedin.svg" alt="linkedin"></a></li>
+							<li><a href="#"><img src="img/socials/github.svg" alt="github"></a></li>
+						</ul>	
+				</div>
+				<div class="footer-contacts">
+					<a href="#" class="footer-button-vk">Связаться Вконтакте</a>
+					<p> Напишите мне, чтобы заказать или узнать стоимость верстки вашего проекта</p>
+</div>
+			</div>
+		</div>
+	</footer>
+	</body>
+</html>
